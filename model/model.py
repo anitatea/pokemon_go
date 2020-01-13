6ad2844b53429c35f333456ef085c3eed383fe35
meow <anita.tran38@gmail.com>
2020-01-13 14:00:31 -0500

    update

--- model/model.py
+++ model/model.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.linear_model import LinearRegression, Lasso, Ridge, LogisticRegression
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn_pandas import DataFrameMapper, CategoricalImputer, FunctionTransformer
 from sklearn.preprocessing import StandardScaler, LabelBinarizer, PolynomialFeatures
 from sklearn.impute import SimpleImputer
@@ -19,6 +18,8 @@ y = df[target]
 X = df.drop(target, axis=1)
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
+df.info()
+
 mapper = DataFrameMapper([
      # ('latitude',[CategoricalImputer(), LabelBinarizer()]),
      # ('longitude',[CategoricalImputer(), LabelBinarizer()]),
@@ -33,78 +34,15 @@ mapper = DataFrameMapper([
 Z_train = mapper.fit_transform(X_train)
 Z_test = mapper.transform(X_test)
 
-# select = SelectPercentile(percentile=40)
-# select.fit(Z_train, y_train)
-# Z_train = select.transform(Z_train)
-# Z_test = select.transform(Z_test)
-
-
-# model = KNeighborsClassifier(n_neighbors=4)
-# model.fit(Z_train, y_train)
-# pd.DataFrame({
-#     'ytrue': y_test,
-#     'yhat': model.predict(Z_test)
-# })
-#
-# model.score(Z_train, y_train), model.score(Z_test, y_test)
-#
-#
-# model.predict_proba(Z_test)
+select = SelectPercentile(percentile=40)
+select.fit(Z_train, y_train)
+Z_train = select.transform(Z_train)
+Z_test = select.transform(Z_test)
 
 
 model = LogisticRegression(solver='lbfgs')
 model.fit(Z_train, y_train)
-predict = model.predict_proba(Z_test)
-
-pipe = make_pipeline(mapper, model)
-pipe.fit(X_train, y_train)
-pipe.score(X_test, y_test)
-pickle.dump(pipe, open('pipe.pkl', 'wb'))
-
-
-
-
-
-
-
-
-
-
-X_train.sample().to_dict(orient='list')
-
-new = pd.DataFrame({
-
- # 'latitude': [34.010027],
- # 'longitude': [-118.49583899999999],
- # 'local_time': ['2016-09-02T22:14:14'],
- 'close_to_water': [True],
- 'city': ['Los_Angeles'],
- 'weather': ['Clear'],
- 'temperature': [19.7],
- 'population_density': [4188.391]
-})
-
-
-y_train.unique().shape
-pipe.predict_proba(new)[0].shape
-
-prediction = float(pipe.predict(new)[0])
-
-
-l = list(df['pokedex_id'].unique())
-l.sort()
-l
-
-
-
-
-
-
-# pd.DataFrame({
-#     'ytrue': y_test,
-#     'yhat': model.predict(Z_test)
-# })
-
+model.predict(X_test)
 
 # model = cb.CatBoostClassifier(
 #     iterations=100,
@@ -118,7 +56,7 @@ l
 #     plot=False,
 # )
 
-# model.score(Z_test, y_test)
+model.score(Z_test, y_test)
 
 
 # pipe = make_pipeline(mapper, model)
