326b0236997153f8d6d333b0242e58322fed8236
meow <anita.tran38@gmail.com>
2020-01-13 11:15:01 -0500

    'hi'

--- app.py
+++ app.py
@@ -14,19 +14,20 @@ def index():
 def predict():
     if request.method == 'POST':
         result = request.form
-    new = pd.DataFrame({
-#        'Make': [result['make']],
-        'Location': [result.get('Location')],
-        'Year': [result.get('Year')],
-        'Fuel_Type': [result.get('Fuel_Type')],
-        'Transmission': [result.get('Transmission')],
-        'Engine': [result.get('Engine')],
-        'Power': [result.get('Power')],
-        'Seats': [result.get('Seats')]
-    })
-    prediction = pipe.predict(new)[0]
-    prediction = '${:,.2f}'.format(prediction)
-    return render_template('result.html', prediction=prediction)
+#     new = pd.DataFrame({
+# #        'Make': [result['make']],
+#         'Location': [result.get('Location')],
+#         'Year': [result.get('Year')],
+#         'Fuel_Type': [result.get('Fuel_Type')],
+#         'Transmission': [result.get('Transmission')],
+#         'Engine': [result.get('Engine')],
+#         'Power': [result.get('Power')],
+#         'Seats': [result.get('Seats')]
+#     })
+    # prediction = pipe.predict(new)[0]
+    # prediction = '${:,.2f}'.format(prediction)
+    # return render_template('result.html', prediction=prediction)
+    return render_template('result.html', prediction=result['pokedex'])
 
 if __name__ == '__main__':
     app.run(debug=True) #(debug=True), remove this when everything has been built
