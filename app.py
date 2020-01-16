1e19a8c80bc18a43999e031f8e4247098a73edbb
finnreilly@tuta.io <26311585+Vadric@users.noreply.github.com>
2020-01-16 14:32:28 -0500

    update

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def weather(lat,long):
     return [ weather_codes[f'{weather_id}'] , weather_temp ]
 
 def get_my_ip():
-    ip =  '8.8.8.8'#request.headers.get('X-Forwarded-For', request.remote_addr) 
+    ip =  '72.143.53.170'#request.headers.get('X-Forwarded-For', request.remote_addr) 
     base_url = "https://api.ipgeolocation.io/astronomy?apiKey="
     key = "2e571f58a33c4c92b5b9e83a95d4d554"
     complete_url = base_url + key + "&ip=" + str(ip) + "&lang=en"
@@ -40,75 +40,79 @@ def get_my_ip():
     city = x['location']['city']
     latitude_ip = x['location']['latitude']
     longitude_ip = x['location']['longitude']
-    return [city, latitude_ip, longitude_ip]
+    base_url_2 = "https://api.ipgeolocation.io/timezone?apiKey=" 
+    complete_url_2 = base_url_2 + key + "&ip=" + str(ip) + "&lang=en"
+    response = requests.get(complete_url_2)
+    x = response.json()
+    local_timezone = x['timezone_offset']
+    return [city, latitude_ip, longitude_ip, local_timezone]
+
+def predict_poke(latitude=0,longitude=0, local_timezone=0):
+    if latitude==0 and longitude==0 and local_timezone==0:
+        city, latitude, longitude, local_timezone = get_my_ip()
+
+    weather_location, temperature = weather(latitude, longitude)
+    local_hour = int(time.strftime("%H", time.gmtime())) + local_timezone
+    local_day = str(time.strftime("%A", time.gmtime()))
+    new = pd.DataFrame({ # should be based on the options of the model
+            'latitude' : [latitude],
+            'longitude' : [longitude],
+            'hour' : [local_hour],
+            'day' : [local_day],
+            'close_to_water': ['yes'],
+            'weather': [weather_location],
+            'temperature': [temperature],
+            'population_density': 4188.391 # [result.get('population_density')]
+    })
+    l = list(pipe.predict_proba(new))
+    zip_list = list(zip(pipe.classes_,l[0]))
+    df1 = pd.DataFrame(zip_list, columns=['id','prob'])
+    # Reading in a DataFrame that has poke ids with there pokemon names
+    df2 = pd.read_csv('data/poke.csv')
+    # matching the ids then appending the name into an empty list
+    new_list = []
+    for row in range(len(df1)):
+        for i in range(len(df2)):
+            if df1['id'][row] == df2['id'][i]:
+                new_list.append(df2['pokemon'][i])
+        else:
+            pass
+    # putting the list of names into df1
+    df1['pokemon'] = new_list
+    out = df1.sort_values(by=['prob'], ascending=False)[['id','pokemon']]
+    out2 = out.reset_index(drop=True).to_dict(orient='dict')
+    return out2
 
 @app.route('/', methods=['POST','GET'])
 def index():
-    city, latitude, longitude = get_my_ip()
-    weather_location, temperature = weather(latitude, longitude)
-    local_hour = int(time.strftime("%H", time.gmtime())) #+ offset
-    local_hour
-    local_day = int(time.strftime("%-d", time.gmtime()))
-    local_day
-    if request.method == 'POST':
-        result = request.form
-    new = pd.DataFrame({ # should be based on the options of the model
-         'latitude' : [latitude],
-         'longitude' : [longitude],
-         'hour' : [local_hour],
-         'day' : ['Monday'],
-         'close_to_water': ['yes'],
-         'weather': [weather_location],
-         'temperature': [temperature],
-         'population_density': 4188.391 # [result.get('population_density')]
-    })
+    proba_dict = predict_poke() 
+    return render_template('index.html',  gif=proba_dict['id'], names=proba_dict['pokemon'])
 
-    l = pipe.predict_proba(new)
-    df1 = pd.read_csv('data/poke.csv')
-    df2 = pd.DataFrame(l.reshape(-1,1), columns=['prob'])
-    df2
-    df3 = pd.concat([df1,df2],axis=1)
-    out = df3.sort_values(by=['prob'], ascending=True).head(10)[['id','pokemon']].reset_index()
-    #out2 = out.to_dict(orient='split')
+@app.route('/north', methods=['POST','GET'])
+def north():
+    city, latitude, longitude, local_timezone = get_my_ip()
+    proba_dict = predict_poke(latitude+.03, longitude, local_timezone)
+    return render_template('index.html',  gif=proba_dict['id'], names=proba_dict['pokemon'])
 
-    poke_0 = out['pokemon'][0]
-    gif_0 = out['id'][0]
-    poke_1 = out['pokemon'][1]
-    gif_1 = out['id'][1]
-    poke_2 = out['pokemon'][2]
-    gif_2 = out['id'][2]
-    poke_3 = out['pokemon'][3]
-    gif_3 = out['id'][3]
-    poke_4 = out['pokemon'][4]
-    gif_4 = out['id'][4]
-    poke_5 = out['pokemon'][5]
-    gif_5 = out['id'][5]
-    poke_6 = out['pokemon'][6]
-    gif_6 = out['id'][6]
-    poke_7 = out['pokemon'][7]
-    gif_7 = out['id'][7]
-    poke_8 = out['pokemon'][8]
-    gif_8 = out['id'][8]
-    poke_9 = out['pokemon'][9]
-    gif_9 = out['id'][9]
+@app.route('/south', methods=['POST','GET'])
+def south():
+    city, latitude, longitude, local_timezone = get_my_ip()
+    proba_dict = predict_poke(latitude-.03, longitude, local_timezone)
+    return render_template('index.html',  gif=proba_dict['id'], names=proba_dict['pokemon'])
 
+@app.route('/east', methods=['POST','GET'])
+def east():
+    city, latitude, longitude, local_timezone = get_my_ip()
+    proba_dict = predict_poke(latitude, longitude-.03, local_timezone)
+    return render_template('index.html',  gif=proba_dict['id'], names=proba_dict['pokemon'])
 
+@app.route('/west', methods=['POST','GET'])
+def west():
+    city, latitude, longitude, local_timezone = get_my_ip()
+    proba_dict = predict_poke(latitude, longitude+.03, local_timezone)
+    return render_template('index.html',  gif=proba_dict['id'], names=proba_dict['pokemon'])
 
-    return render_template('result_test.html',  poke_0=poke_0,gif_0=gif_0,poke_1=poke_1,gif_1=gif_1,poke_2=poke_2,gif_2=gif_2,poke_3=poke_3,gif_3=gif_3,poke_4=poke_4,gif_4=gif_4,poke_5=poke_5,gif_5=gif_5,poke_6=poke_6,gif_6=gif_6,poke_7=poke_7,gif_7=gif_7,poke_8=poke_8,gif_8=gif_8,poke_9=poke_9,gif_9=gif_9)
-  #  def render_sidebar_template(tmpl_name, **kwargs):
-  #  (var1, var2, var3) = generate_sidebar_data()
-  #  return render_template(tmpl_name, var1=var1, var2=var2, var3=var3, **kwargs)
-# df1 = pd.read_csv('data/poke.csv')
-# l = list(model.predict_proba(Z_test)[8])
-# df2 = pd.DataFrame(l,columns=['prob'])
-# df3 = pd.concat([df1,df2],axis=1)
-# list(df3.sort_values(by=['prob'], ascending=False).head(10)['pokemon'])
-# df3.sort_values(by=['prob'], ascending=False).head(10)
-
-
-
-
-
+'''
 @app.route('/result', methods=['POST','GET'])
 def manual_result():
         if request.method == 'POST':
@@ -132,6 +136,6 @@ def manual_result():
 @app.route('/manual')
 def manual_entry():
     return render_template('manual.html')
-
+'''
 if __name__ == '__main__':
     app.run(debug=True) #(debug=True), remove this when everything has been built
